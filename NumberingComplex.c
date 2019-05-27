08e04db83f4935409b5ffddf420569fbc9ca4086
zwondd <zwondd@gmail.com>
2019-05-28 00:29:56 +0900

    NumberingComplex BFS

--- NumberingComplex.c
+++ NumberingComplex.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // 19.05.24
 // 백준 2667번 : 단지번호붙이기
+// BFS 방식
+
+// - 단지 수 최대 N^2/2 개
+// - sort 알고리즘 다시 공부하기 
 
 int N;
 int arr[30][30]={0,};
-
 int visited[30][30]={0,};
-int complex_arr[10000]={0,};
-int complex_num=1;
+int complex_arr[1000]={0,};
+int complex_num=0;
 
 int pos_x[4]={-1,0,1,0};
 int pos_y[4]={0,1,0,-1};
 
-int q_x[30];
-int q_y[30];
-
 int front=0;
 int rear=0;
+
+struct Pos{
+    int x,y;
+};
+struct Pos q[1000];
 
 void Input_Data(void)
 {
@@ -35,41 +41,29 @@ void Input_Data(void)
 
 void bfs(int complex, int x,int y) 
 {
-    q_x[rear++]=x;
-    q_y[rear++]=y;
+    struct Pos cur={x,y};
+    q[rear++]=cur;
+    visited[x][y]=1;
+    complex_arr[complex]++;
 
     while ( front!= rear )
     {
         int i;
-
-        int cur_x=q_x[front++];
-        int cur_y=q_y[front++];
-
-        if( !visited[cur_x][cur_y] )
-        {
-            // printf("x:%d, y:%d \n",cur_x,cur_y);
-            visited[cur_x][cur_y]=1;
-            complex_arr[complex]++;
-        }
-
+        struct Pos cur=q[front++];
+        int cur_x=cur.x;
+        int cur_y=cur.y;
+        
         for( i=0; i<4; i++ )
         {
             int next_x=cur_x+pos_x[i];
             int next_y=cur_y+pos_y[i];
 
-            if( next_x<0 || next_x>=N || next_y<0 || next_y>=N || visited[next_x][next_y]==1 )
-            {
-                continue;
-            }
-            else if( !arr[next_x][next_y] )
+            if( next_x>=0 && next_x<N && next_y>=0 && next_y<N && arr[next_x][next_y] && !visited[next_x][next_y]  )
             {
                 visited[next_x][next_y]=1;
-                continue;
-            }
-            else
-            {
-                q_x[rear++]=next_x;
-                q_y[rear++]=next_y;
+                complex_arr[complex]++;
+                struct Pos next={next_x,next_y};
+                q[rear++]=next;
             }
         }
     }
@@ -92,15 +86,17 @@ void solve()
 
 void sort()
 {
-    int i,tmp;
-    for(i=1; i<complex_num-1; i++)
+    int i,j,min;
+    for(i=0; i<complex_num-1; i++)
     {
-        if ( complex_arr[i] > complex_arr[i+1] ) 
+        min=i;
+        for(j=i+1;j<complex_num;j++)
         {
-            tmp=complex_arr[i+1];
-            complex_arr[i+1]=complex_arr[i];
-            complex_arr[i]=tmp;
+            min=(complex_arr[j]<complex_arr[min]) ? j:min;
         }
+        int tmp=complex_arr[i];
+        complex_arr[i]=complex_arr[min];
+        complex_arr[min]=tmp;
     }
 }
 
@@ -110,11 +106,10 @@ int main(void)
     int i;
 
     Input_Data();
-
     solve();
     sort();
 
-    printf("%d\n",complex_num-1);
-    for(i=1;i<complex_num;i++)
-        printf("%d\n",complex_arr[i]);
+    printf("%d",complex_num);
+    for(i=0;i<complex_num;i++)
+        printf("\n%d",complex_arr[i]);
 }
