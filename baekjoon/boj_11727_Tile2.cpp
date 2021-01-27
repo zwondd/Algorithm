0740353bfa3e8f9e5e4039fee58745c605f78a37
zwondd <zwondd@gmail.com>
2021-01-27 23:07:59 +0900

    [bj] tiling2_other solution

--- baekjoon/boj_11727_Tile2.cpp
+++ baekjoon/boj_11727_Tile2.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 #define MOD 10007
+
+#if 0
 int arr[1010];
 
 int solve(int n) {
@@ -16,10 +18,25 @@ int solve(int n) {
 
 	return ret = (solve(n-2)*2 + solve(n-1)) % MOD;
 }
-
 int main(void) {
 	int tc;
 	memset(arr, -1, sizeof(arr));
+	cin >> tc;
+	cout << solve(tc) << endl;
+}
+#endif
+
+int arr[1010]={0,1,3};
+
+
+int solve(int n) {
+	int& ret = arr[n];
+	if ( ret ) return ret;
+	return ret = (solve(n-2)*2 + solve(n-1)) % MOD;
+}
+
+int main(void) {
+	int tc;
 	cin >> tc;
 	cout << solve(tc) << endl;
 }
