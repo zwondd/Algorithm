ce0dd485551fd0c3999718857bc82e4b3992d94c
zwondd <zwondd@gmail.com>
2022-01-10 23:03:51 +0900

    [leetcode] ksmallestPairs - using priority queue(heap, full binary tree)

--- leetcode/medium/373_FindPairsWithSmallestSums.java
+++ leetcode/medium/373_FindPairsWithSmallestSums.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 /*
     2022-01-06
     [Leetcode][Medium] 373. Find K Pairs with Smallest Sums
@@ -7,116 +5,44 @@ import java.util.ArrayList;
 import java.util.*;
 
 class FindPairsWithSmallestSums {
+    /*
+        Runtime: 30 ms, faster than 19.04% of Java online submissions for Find K Pairs with Smallest Sums.
+        Memory Usage: 109.8 MB, less than 5.11% of Java online submissions for Find K Pairs with Smallest Sums.
+        ref) https://www.programmerall.com/article/196979118/
+    */
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        List<List<Integer>> pairs = new ArrayList<>();
-        List<Integer> smaller;
-        
-        if ( nums1.length * nums2.length <= k ) {
-            for(int i=0; i<nums1.length; i++) {
-                for(int j=0; j<nums2.length; j++) {
-                    smaller = new ArrayList<>();
-                    smaller.add(nums1[i]);
-                    smaller.add(nums2[j]);
-                    pairs.add(smaller);
-                }
-            }
-            return pairs;
+        // using priority queue (heap)
+
+        List<List<Integer>> res = new ArrayList<>();
+        if ( nums1.length == 0 || nums2.length == 0 ) {
+            return res;
         }
-
-        int posX=0;
-        int posY=0;
-
-        List<Integer> tmp = new ArrayList<>();
-        tmp.add(nums1[posX]);
-        tmp.add(nums2[posY]);
-        pairs.add(tmp);
-        
-        boolean increaseX = false;
-        boolean increaseY = false;
-
-        while( pairs.size() < k ) {
-            smaller = new ArrayList<>();
-            //System.out.println(nums1[posX] + " -- " + nums2[posY] );
-            
-            if ( posX+1>=nums1.length && posY+1>=nums2.length ) {
-                break;
-            }
-            if ( posX+1>=nums1.length ) {
-                smaller.add(nums1[posX]);
-                smaller.add(nums2[++posY]);
-                pairs.add(smaller);
-                continue;
-            }
-
-            if ( posY+1>=nums2.length ) {
-                smaller.add(nums1[++posX]);
-                smaller.add(nums2[posY]);
-                pairs.add(smaller);
-                continue;
-            }
-
-            if ( increaseX && posX-1>=0 ) {
-                if ( nums1[posX-1]+nums2[posY+1] <= nums1[posX+1]+nums2[posY] ) {
-                    smaller.add(nums1[--posX]);
-                    smaller.add(nums2[posY++]);
-                    pairs.add(smaller);
-
-                    increaseX = false;
-                    increaseY = true;
-                    continue;
-                } else {
-                    smaller.add(nums1[++posX]);
-                    smaller.add(nums2[posY]);
-                    pairs.add(smaller);
-
-                    increaseX = true;
-                    increaseY = false;
-                    continue;
-                }
-            }
-
-            if ( increaseY && posY-1>=0 ) {
-                if ( nums1[posX+1]+nums2[posY-1] <= nums1[posX]+nums2[posY+1] ) {
-                    smaller.add(nums1[++posX]);
-                    smaller.add(nums2[--posY]);
-                    pairs.add(smaller);
-
-                    increaseX = true;
-                    increaseY = false;
-                    continue;
-                } else {
-                    smaller.add(nums1[posX]);
-                    smaller.add(nums2[++posY]);
-                    pairs.add(smaller);
-
-                    increaseX = false;
-                    increaseY = true;
-                    continue;
-                }
-            }
-
-            //System.out.println("smaller : " + (nums1[posX+1]+nums2[posY]) + " vs " + (nums1[posX]+nums2[posY+1]) );
-            if ( nums1[posX+1]+nums2[posY] <= nums1[posX]+nums2[posY+1] ) {
-                smaller.add(nums1[++posX]);
-                smaller.add(nums2[posY]);
-                pairs.add(smaller);
-
-                increaseX = true;
-                increaseY = false;
-                continue;
-            } 
-            if ( nums1[posX+1]+nums2[posY] > nums1[posX]+nums2[posY+1] ) {
-                smaller.add(nums1[posX]);
-                smaller.add(nums2[++posY]); 
-                pairs.add(smaller);
-
-                increaseX = false;
-                increaseY = true;
-                continue;
-            }
-            //System.out.println( posX + " .. " + posY );
+        PriorityQueue<int[]> minPq = new PriorityQueue<>((a,b) -> {
+            return nums1[a[0]] + nums2[a[1]] - nums1[b[0]] - nums2[b[1]];
+        });
+        Set<Integer>[] set = new Set[nums1.length];
+        for(int i=0; i<nums1.length; i++) {
+            set[i] = new HashSet<>();
         }
+        minPq.add(new int[]{0,0});
+        set[0].add(0);
+        while( k>0 && minPq.size()>0 ) {
+            int[] curr = minPq.poll();
+            List<Integer> list = new ArrayList<>();
+            list.add(nums1[curr[0]]);
+            list.add(nums2[curr[1]]);
+            res.add(list);
 
-        return pairs;
+            if ( curr[0] < nums1.length-1 && !set[curr[0]+1].contains(curr[1])) {
+                minPq.add(new int[]{curr[0]+1, curr[1]});
+                set[curr[0]+1].add(curr[1]);
+            }
+            if ( curr[1] < nums2.length-1 && !set[curr[0]].contains(curr[1]+1)) {
+                minPq.add(new int[]{curr[0], curr[1]+1});
+                set[curr[0]].add(curr[1]+1);
+            }
+            k--;
+        }
+        return res;
     }
 }
