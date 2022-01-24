bc3c58fe0af2689a4f979820189306a6a773467f
zwondd <zwondd@gmail.com>
2022-01-24 23:02:32 +0900

    [leetcode] collections to list

--- leetcode/medium/49_GroupAnagrams.java
+++ leetcode/medium/49_GroupAnagrams.java
@@ -28,7 +28,14 @@ class GroupAnagrams {
         for(List<String> keyList : key.values() ) {
             anagram.add(keyList);
         }
-
         return anagram;
+
+
+        /*
+            Runtime: 14 ms, faster than 48.88% of Java online submissions for Group Anagrams.
+            Memory Usage: 53.1 MB, less than 36.15% of Java online submissions for Group Anagrams.
+        */
+        // List<List<String>> aa = new LinkedList<>(key.values());
+        // return aa;
     }
 }
